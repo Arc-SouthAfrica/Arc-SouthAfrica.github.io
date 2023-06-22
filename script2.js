bf608b50f818e87e1b25d601a175160988e1c381
Rethar Osman Abdullah <retharyt@gmail.com>
2023-06-22 21:45:37 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,9 +1,35 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var navbarToggler = document.querySelector('.navbar-toggler');
-    var navbarNav = document.querySelector('.navbar-nav');
-    
-    navbarToggler.addEventListener('click', function() {
-      navbarNav.classList.toggle('active');
-    });
-  });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500); // Change image every "" seconds
+}
+
+var scrollTopButton = document.getElementById('scroll-top-button');
+var scrollTimeout;
+
+window.addEventListener('scroll', function() {
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(function() {
+    scrollTopButton.style.opacity = '0'; 
+  }, 2000);
   
+  if (window.pageYOffset > 0) {
+    scrollTopButton.style.opacity = '1'; 
+  } else {
+    scrollTopButton.style.opacity = '0'; 
+  }
+});
+
+scrollTopButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
